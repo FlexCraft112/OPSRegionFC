9b8977376f61c8cc0df26afccfc820f633011853
FlexCraft112 <159067201+FlexCraft112@users.noreply.github.com>
2026-01-22 07:07:04 +0100

    Create OPSRegionNG.java

---
+++ src/main/java/me/flexcraft/opsregionng/OPSRegionNG.java
@@ -0,0 +1,32 @@
+package me.flexcraft.opsregionng;
+
+import me.flexcraft.opsregionng.listener.BlockProtectionListener;
+import me.flexcraft.opsregionng.listener.WorldEditCommandListener;
+import me.flexcraft.opsregionng.listener.RegionProtectListener;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class OPSRegionNG extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        saveDefaultConfig();
+
+        Bukkit.getPluginManager().registerEvents(
+                new BlockProtectionListener(this), this
+        );
+        Bukkit.getPluginManager().registerEvents(
+                new WorldEditCommandListener(this), this
+        );
+        Bukkit.getPluginManager().registerEvents(
+                new RegionProtectListener(this), this
+        );
+
+        getLogger().info("OPSRegionNG enabled");
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("OPSRegionNG disabled");
+    }
+}
